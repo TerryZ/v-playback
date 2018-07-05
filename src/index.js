40c37b3c58f15c419d5703ab61f52a2caf6cbe45
TerryZ <terry5@foxmail.com>
2018-07-05 22:01:51 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import video from './Video';
+
+const Plugin = {
+    install(Vue, options = {}){
+        if(Object.keys(options).length){
+            if(typeof(options.language) === 'string') video.props.language.default = options.language;
+            if(typeof(options.loop) === 'boolean') video.props.loop.default = options.loop;
+            if(typeof(options.autoPlay) === 'boolean') video.props.autoPlay.default = options.autoPlay;
+        }
+        Vue.component(video.name, video);
+    }
+};
+
+export default Plugin;
